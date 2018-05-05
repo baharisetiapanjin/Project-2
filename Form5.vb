593928fc6cc43a7e05c41c6e2aee9e82595a1f57
baharisetiapanjin <38752009+baharisetiapanjin@users.noreply.github.com>
2018-05-05 10:31:08 +0700

    Add files via upload

---
+++ Form5.vb
@@ -0,0 +1,40 @@
+﻿Public Class Form5
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Dim a, b, c, i, faktor, fibbo, pangkat As Integer
+        a = TextBox1.Text
+
+        faktor = 1
+        For i = 1 To a
+            faktor = i * faktor
+            TextBox2.Text = faktor
+        Next
+
+        fibbo = 0
+        b = 1
+        c = -1
+        For i = 0 To a
+            fibbo = b + c
+            c = b
+            b = fibbo
+            TextBox3.Text = fibbo
+        Next
+
+        pangkat = 1
+        For i = 1 To a
+            pangkat = a * a
+            TextBox4.Text = pangkat
+        Next
+
+    End Sub
+
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        TextBox1.Focus()
+        TextBox1.Clear()
+        TextBox2.Clear()
+        TextBox3.Clear()
+        TextBox4.Clear()
+
+    End Sub
+End Class
